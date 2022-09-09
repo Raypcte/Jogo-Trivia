78a0f86977c9bffb2a88925416231b6f7df17dee
Diogo Sales <diogosales.ads@gmail.com>
2022-09-09 20:19:39 -0300

    [group-27]-req-14-16

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,15 +1,21 @@
-import { FETCH_SCORE } from '../actions';
+import { FETCH_SCORE, FETCH_ASSERTIONS } from '../actions';
 
 const initialState = {
   score: 0,
+  assertions: 0,
 };
 
-const triviaReducer = (state = initialState, { type, score }) => {
+const triviaReducer = (state = initialState, { type, score, assertions }) => {
   switch (type) {
   case FETCH_SCORE:
     return {
       ...state,
       score,
+    };
+  case FETCH_ASSERTIONS:
+    return {
+      ...state,
+      assertions: state.assertions + assertions,
     };
   default:
     return {
